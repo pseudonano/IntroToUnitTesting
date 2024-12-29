65b129141d2b499cf571371d9c5415426feb29ec
pseudonano <teguhpambudi1251@gmail.com>
2024-12-30 01:41:14 +0700

    junit initial commit

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,11 @@
+/**
+ * The class containing your tests for the {@link Demo} class.  Make sure you
+ * test all methods in this class (including both 
+ * {@link Demo#main(String[])} and 
+ * {@link Demo#isTriangle(double, double, double)}).
+ */
+public class DemoTest {
+	String input = "1\n";
+	
+	ByteArrayInputStream in = new ByteArrayInputStream(input.)
+}
